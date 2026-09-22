435282013177f5b4cdbf1e45e38a3f9aa156a3b9
Ese Omame <e.omame7@gmail.com>
2026-09-22 13:15:09 -0700

    Re-upload

--- sw.js
+++
@@ -1,7 +0,0 @@
-
-const CACHE_NAME='pips-co-v62-nofirebase';
-self.addEventListener('install', e=>{self.skipWaiting();});
-self.addEventListener('activate', e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
-});
-self.addEventListener('fetch', e=>{});
