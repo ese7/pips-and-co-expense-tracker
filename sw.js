96639d866a05d90e6bcba5e27b5e1b3a261b71ca
Ese Omame <e.omame7@gmail.com>
2026-09-22 13:44:10 -0700

    Re-upload

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE_NAME='v63-logofix'; self.addEventListener('install',e=>{self.skipWaiting();}); self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.map(c=>caches.delete(c)))).then(()=>self.clients.claim()));}); self.addEventListener('fetch',e=>{});
